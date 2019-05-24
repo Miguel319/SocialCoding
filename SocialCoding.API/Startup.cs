c73425003ce7adf20912fb3dcfb270f6e4297567
Miguel319 <miguelp90.319@gmail.com>
2019-05-24 17:16:23 -0400

    Creación de la UI, CORS & peticiones al servidor desde Angular

--- SocialCoding.API/Startup.cs
+++ SocialCoding.API/Startup.cs
@@ -21,6 +21,7 @@ namespace SocialCoding.API
                 x => x.UseSqlite(
                     Configuration.GetConnectionString("ConexionDb")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace SocialCoding.API
             } 
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
