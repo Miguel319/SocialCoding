991a518c3de9940176a99bc75c1efc8c8a066ca3
Miguel319 <miguelp90.319@gmail.com>
2019-05-24 14:31:39 -0400

    Pruebas de la bd

---
+++ SocialCoding.API/Data/SocialCodingContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SocialCoding.API.Models;
+
+namespace SocialCoding.API.Data
+{
+    public class SocialCodingContext : DbContext
+    {
+        public SocialCodingContext(DbContextOptions<SocialCodingContext> options) : base(options) {}
+
+        public DbSet<Valor> Valores { get; set; }
+
+        
+    }
+}
---
+++ SocialCoding.API/Models/Valor.cs
@@ -0,0 +1,8 @@
+namespace SocialCoding.API.Models
+{
+    public class Valor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
